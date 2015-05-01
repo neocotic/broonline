32c5f27fc9b09f07a419ed67a006b622f025bc9c
Alasdair Mercer <mercer.alasdair@gmail.com>
2015-05-01 16:30:09 +0100

    added newrelic for monitoring

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('newrelic');
+
 var express = require('express');
 var kraken = require('kraken-js');
 
---
+++ newrelic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.config = {
+    app_name: ['thebroonline'],
+    license_key: process.env.NEW_RELIC_LICENSE_KEY,
+    logging: {
+        level: 'info'
+    }
+};
