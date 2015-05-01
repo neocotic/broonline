7c0288600cfac6e1bc4e2b99b8a964b76100afef
Alasdair Mercer <mercer.alasdair@gmail.com>
2015-05-01 19:38:20 +0100

    consolidated server.js and index.js

--- index.js
+++ index.js
@@ -5,23 +5,17 @@ require('newrelic');
 var express = require('express');
 var kraken = require('kraken-js');
 
-/*
- * Create and configure application. Also exports application instance for use by tests.
- * See https://github.com/krakenjs/kraken-js#options for additional configuration options.
- */
 var options = {
     onconfig: function(config, next) {
-        /*
-         * Add any additional config setup or overrides here. `config` is an initialized
-         * `confit` (https://github.com/krakenjs/confit/) configuration object.
-         */
         next(null, config);
     }
 };
+var port = process.env.PORT || 8000;
 
 var app = module.exports = express();
 app.use(kraken(options));
 app.on('start', function() {
-    console.log('Application ready to serve requests.');
-    console.log('Environment: %s', app.kraken.get('env:env'));
+    app.listen(port, function() {
+        console.log('Listening on port %d for %s', port, app.kraken.get('env:env'));
+    });
 });
