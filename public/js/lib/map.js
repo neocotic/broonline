4ae3dd05b59631dac002bdc3fc8d5d6a1d658f6c
Alasdair Mercer <mercer.alasdair@gmail.com>
2015-05-05 11:51:41 +0100

    now always centres on Scotland by default

--- public/js/lib/map.js
+++ public/js/lib/map.js
@@ -5,34 +5,15 @@ define([
     './google-maps'
 ], function($, gmaps) {
 
-    var DEFAULT_POSITION = new gmaps.LatLng(55.9410656, -3.2053836);
-
     function Map(options) {
         this.$el = options.$el;
         this.el = this.$el[0];
 
         this.map = new gmaps.Map(this.el, {
+            center: new gmaps.LatLng(55.9410656, -3.2053836),
             zoom: 8
         });
-
-        this.getInitialPosition = this.getInitialPosition($.proxy(function(position) {
-            this.map.setCenter(position);
-        }, this));
     }
-
-    Map.prototype.getInitialPosition = function(callback) {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                function(position) {
-                    callback(new gmaps.LatLng(position.coords.latitude,  position.coords.longitude));
-                },
-                function() {
-                    callback(DEFAULT_POSITION);
-                });
-        } else {
-            callback(DEFAULT_POSITION);
-        }
-    };
 
     return Map;
 
